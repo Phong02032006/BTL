3ac7a8ff838e9b9f01912d5268d700f9b4a5eaa5
Nguyễn  Phong <24022823@vnu.edu.vn>
2025-03-03 15:17:24 +0700

    2

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <SDL.h>
+#include "Paddle.h"
+
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    SDL_Window* window = SDL_CreateWindow("Paddle Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+        SDL_RENDERER_ACCELERATED);
+
+    // Tạo paddle ở giữa bên trái màn hình
+    Paddle paddle(20, SCREEN_HEIGHT / 2 - Paddle::PADDLE_HEIGHT / 2, SCREEN_HEIGHT);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+
+        // Xử lý input
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        if (state[SDL_SCANCODE_W]) paddle.move(true);
+        if (state[SDL_SCANCODE_S]) paddle.move(false);
+
+        // Clear và vẽ
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        paddle.render(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); // ~60 FPS
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
---
+++ src/paddle.cpp
@@ -0,0 +1,26 @@
+#include "paddle.h"
+#include <algorithm>
+Paddle::Paddle(int x, int y, int sh) :screenHeight(sh) {
+	rect.x = x;
+	rect.y = y;
+	rect.w = PADDLE_WIDTH;
+	rect.h = PADDLE_HEIGHT;
+}
+
+void Paddle::move(bool up) {
+	if (up) {
+		rect.y -= PADDLE_SPEED;
+	}
+	else {
+		rect.y += PADDLE_SPEED;
+	}
+	rect.y = std::max(0, std::min(screenHeight - PADDLE_HEIGHT, rect.y));
+}
+
+void Paddle::render(SDL_Renderer* renderer) const {
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderFillRect(renderer, &rect);
+}
+SDL_Rect& Paddle::getRect() {
+	return rect;
+}
