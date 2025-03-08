b9f5c4e26e33e65e422d22c24d2052b84d35728d
Nguyễn  Phong <24022823@vnu.edu.vn>
2025-03-08 15:34:53 +0700

    thêm 2 paddles

--- src/main.cpp
+++ src/main.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
         SDL_RENDERER_ACCELERATED);
 
     // Tạo paddle ở giữa bên trái màn hình
-    Paddle paddle(20, SCREEN_HEIGHT / 2 - Paddle::PADDLE_HEIGHT / 2, SCREEN_HEIGHT);
-
+    Paddle left_paddle(20, SCREEN_HEIGHT / 2 - Paddle::PADDLE_HEIGHT / 2, SCREEN_HEIGHT);
+	Paddle right_paddle(SCREEN_WIDTH - 20 - Paddle::PADDLE_WIDTH, SCREEN_HEIGHT / 2 - Paddle::PADDLE_HEIGHT / 2, SCREEN_HEIGHT);
     bool running = true;
     while (running) {
         SDL_Event event;
@@ -28,14 +28,18 @@ int main(int argc, char* argv[]) {
 
         // Xử lý input
         const Uint8* state = SDL_GetKeyboardState(NULL);
-        if (state[SDL_SCANCODE_W]) paddle.move(true);
-        if (state[SDL_SCANCODE_S]) paddle.move(false);
+        if (state[SDL_SCANCODE_W]) left_paddle.move(true);
+        if (state[SDL_SCANCODE_S]) left_paddle.move(false);
+		if (state[SDL_SCANCODE_UP]) right_paddle.move(true);
+		if (state[SDL_SCANCODE_DOWN]) right_paddle.move(false);
+
 
         // Clear và vẽ
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        paddle.render(renderer);
+        left_paddle.render(renderer);
+		right_paddle.render(renderer);
 
         SDL_RenderPresent(renderer);
 
@@ -45,4 +49,5 @@ int main(int argc, char* argv[]) {
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+    return 0;
 }
--- src/paddle.h
+++ src/paddle.h
@@ -1,14 +1,14 @@
 #include "SDl.h"
 class Paddle {
 public:
-		static const int PADDLE_WIDTH = 15;
-		static const int PADDLE_HEIGHT = 90;
-		static const int PADDLE_SPEED = 10;
-		Paddle(int x, int y, int screenHeight);
-		void move(bool up);
-		void render(SDL_Renderer* renderer) const;
-		SDL_Rect& getRect();
+    static const int PADDLE_WIDTH = 15;
+    static const int PADDLE_HEIGHT = 90;
+    static const int PADDLE_SPEED = 10;
+    Paddle(int x, int y, int screenHeight);
+    void move(bool up);
+    void render(SDL_Renderer* renderer) const;
+    SDL_Rect& getRect();
 private:
-	SDL_Rect rect;
-	int screenHeight;
+    SDL_Rect rect;
+    int screenHeight;
 };
